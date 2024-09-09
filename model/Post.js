d6c0806f56be2b386ca50f4dd2904e87ac0f8624
Matias Hynnä <hynna.matias@gmail.com>
2024-09-09 13:41:27 +0300

    Add new post schema

---
+++ model/Post.js
@@ -0,0 +1,33 @@
+import { Schema, model } from 'mongoose';
+
+const flightNumbersSchema = new Schema({
+  speed: { type: Number, required: true },
+  glide: { type: Number, required: true },
+  stability: { type: Number, required: true },
+  fade: { type: Number, required: true },
+});
+
+const postSchema = new Schema({
+  title: { type: String, required: true },
+  plastic: { type: String, required: true },
+  flightNumbers: {
+    type: flightNumbersSchema,
+    required: true,
+  },
+  images: {
+    type: [String],
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = model('Post', postSchema);
+export default Post;
